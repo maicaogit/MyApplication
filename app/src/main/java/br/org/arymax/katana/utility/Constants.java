6bf7d7aa22bf768e10b467472133be7c070121a4
maicaogit <marcoantoniomg90@gmail.com>
2016-10-15 12:45:13 -0300

    Criação de uma classe de constantes utilitárias e começando a trabalhar com SharedPreferences

---
+++ app/src/main/java/br/org/arymax/katana/utility/Constants.java
@@ -0,0 +1,10 @@
+package br.org.arymax.katana.utility;
+
+/**
+ * Criado por Marco em 15/10/2016.
+ */
+public class Constants {
+
+    public static final String PREFERENCES = "br.org.arymax.katana.preferences";
+
+}
